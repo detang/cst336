fa6047d342191f67139fd3d43225fbb2b822dfb2
Dennis Tang <detang@csumb.edu>
2018-11-13 07:33:39 +0000

    Finished homework 2

--- Homeworks/homework2/blackjack/blackjack.php
+++ Homeworks/homework2/blackjack/blackjack.php
@@ -11,8 +11,8 @@
         <?php
         include 'inc/functions.php';
         
-        play();
+        start();
         
-        ?>
+        ?>        
     </body>
 </html>
--- Homeworks/homework2/blackjack/inc/functions.php
+++ Homeworks/homework2/blackjack/inc/functions.php
@@ -1,17 +1,47 @@
 <?php
         
-        $usedCard = array();
-        
-        function play(){
+        function start(){
                 echo '<div id="player">';
                 
                 playerHand($usedCard);
                 
-                echo'</div>
-                        <div class = "container">
-                        <a class="button-one" title="Relevant Title" href="#">Hit!</a>
-                        <a class="button-two" title="Relevant Title" href="#">Stand!</a>
-                        </div>';
+                
+                echo'</div>';
+        }
+        
+        function cardCount($card1, $card2){
+                
+                $count1 = substr($card1, 0, -1);
+                $count2 = substr($card2, 0, -1);
+                
+
+                
+                if ($count1 == "A"){
+                        $count1 = 1;
+                }
+                elseif ($count1 == "J"){
+                        $count1 = 10;
+                }
+                elseif ($count1 == "Q"){
+                        $count1 = 10;
+                }
+                elseif ($count1 == "K"){
+                        $count1 = 10;
+                }
+                elseif ($count2 == "A"){
+                        $count2 = 1;
+                }
+                elseif ($count2 == "J"){
+                        $count2 = 10;
+                }
+                elseif ($count2 == "Q"){
+                        $count2 = 10;
+                }
+                elseif ($count2 == "K"){
+                        $count2 = 10;
+                }
+
+                return $count1 + $count2;
         }
         
         //***************************************************************************************
@@ -23,19 +53,10 @@
                         "Hearts"        => array("AH", "2H", "3H", "4H", "5H", "6H", "7H", "8H", "9H", "10H", "JH", "QH", "KH"),
                         "Spades"        => array("AS", "2S", "3S", "4S", "5S", "6S", "7S", "8S", "9S", "10S", "JS", "QS", "KS"),
                 );
-                        
+                
+                shuffle($deck);
                 $pattern = array_rand($deck);                           //here you will get a random suit
                 $card = $deck[$pattern][array_rand($deck[$pattern])];           //generate random value within suit 
-                
-                echo $card;
-
-                
-                // if (in_array("Glenn", $people)) {
-                //   echo "Match found";
-                // }
-                // else {
-                //   echo "Match not found";
-                // }
                 
                 return $card;
         }
@@ -44,17 +65,57 @@
                 echo "<img id='card' src='img/$card.png' alt='$pattern'>";
         }
         
-        function playerHand($usedCard) {
+        function playerHand() {
+                
+                $neededcards = Array();
 
                 $dealCard1 = pickCard();
-                $usedCard[] = $dealCard1;
                 
                 $dealCard2 = pickCard();
-                $usedCard[] = $dealCard2;
-
+                
+                while ($dealcard1 != $dealcard2) {
+                        $dealCard2 = pickCard();
+                }
+                
+                array_push($neededcards, $dealCard1);
+                array_push($neededcards, $dealCard2);                
                 displayCard($dealCard1);
                 displayCard($dealCard2);
                 
+                $score = cardCount($dealCard1, $dealCard2);
+                
+                $hitCount = 0;
+                
+                while($score <= 21){
+                        $hitCount = $hitCount + 1;
+                        $hitCard = pickCard();
+                        array_push($neededcards, $hitCard);
+                        displayCard($hitCard);
+                        $score = $score + cardCount(0, $hitCard);
+                        
+                        if ($score > 21){
+                                $finalScore = $score - cardCount(0, $hitCard);
+                        }
+                }
+                
+                if ($finalScore == 21){
+                        echo '<div id="score">';
+                        echo "Your score is $finalScore ";
+                        echo "You Win!";
+                        echo "You only need the first "; 
+                        print_r(count($neededcards) - 1); 
+                        echo " cards</br>";
+                        echo '</div>';   
+                }
+                else {
+                        echo '<div id="score">';
+                        echo "Your score is $finalScore </br>";
+                        echo "It will take $hitCount hit(s) to bust.</br>";
+                        echo "You only need the first "; 
+                        print_r(count($neededcards) - 1); 
+                        echo " cards</br>";
+                        echo '</div>';
+                        
+                }
         }
-
 ?>
